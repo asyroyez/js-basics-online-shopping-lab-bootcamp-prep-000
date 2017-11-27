a412407137bfac9e14a0a53948210c9138db4ac0
Andrey Syroyezhkin <asyroyez@gmail.com>
2017-11-27 18:50:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,39 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  const price = Math.floor(Math.random() * 100) + 1;
+
+  cart.push({[item]: price});
+
+  console.log(`${item} has been added to your cart.`);
+
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  const length = cart.length;
+
+  if (!length) {
+    return console.log("Your shopping cart is empty.");
+  }
+
+  let itemsAndPrices = [];
+
+  for (var i = 0; i < length; i++) {
+    let itemAndPrice = cart[i];
+    let item = Object.keys(itemAndPrice)[0];
+    let price = itemAndPrice[item];
+
+    itemsAndPrices.push(`${item} at $${price}`);
+  }
+
+  switch (itemsAndPrices.length) {
+    case 1:
+      break;
+    case 2:
+      itemsAndPrices = itemsAndPrices.join(" and ");
+      
+  }
 }
 
 function total() {
